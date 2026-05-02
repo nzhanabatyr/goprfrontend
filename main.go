8fec62a132e6d02832a8e9b8dcc7cedffa76a5ae
Nurlan Zhanabatyr <nurlan.zhanabatyr@narxoz.kz>
2026-05-02 06:52:58 +0500

    pract 8 with 10 endpoints and postgres

--- main.go
+++ main.go
@@ -2,19 +2,144 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
+	"goprfrontend/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
+	dsn := "host=localhost user=postgres password=nurlan050 dbname=task_management_db port=5432 sslmode=disable"
 
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
 	}
+
+	fmt.Println("✅ Connected to PostgreSQL!")
+
+	_ = db
+
+	db.AutoMigrate(&models.Task{})
+
+	r := gin.Default()
+
+	r.POST("/tasks", func(c *gin.Context) {
+		var task models.Task
+
+		if err := c.BindJSON(&task); err != nil {
+			c.JSON(400, gin.H{"error": err.Error()})
+			return
+		}
+
+		db.Create(&task)
+
+		c.JSON(200, task)
+
+	})
+
+	r.GET("/tasks", func(c *gin.Context) {
+		var tasks []models.Task
+
+		db.Find(&tasks)
+
+		c.JSON(200, tasks)
+	})
+
+	r.GET("/tasks/:id", func(c *gin.Context) {
+		id := c.Param("id")
+
+		var task models.Task
+
+		if err := db.First(&task, id).Error; err != nil {
+			c.JSON(404, gin.H{"error": "Task not found"})
+			return
+		}
+
+		c.JSON(200, task)
+	})
+
+	r.PUT("/tasks/:id", func(c *gin.Context) {
+		id := c.Param("id")
+
+		var task models.Task
+
+		if err := db.First(&task, id).Error; err != nil {
+			c.JSON(404, gin.H{"error": "Task not found"})
+			return
+		}
+
+		if err := c.BindJSON(&task); err != nil {
+			c.JSON(400, gin.H{"error": err.Error()})
+			return
+		}
+
+		db.Save(&task)
+
+		c.JSON(200, task)
+	})
+
+	r.DELETE("/tasks/:id", func(c *gin.Context) {
+		id := c.Param("id")
+
+		var task models.Task
+
+		if err := db.First(&task, id).Error; err != nil {
+			c.JSON(404, gin.H{"error": "Task not found"})
+			return
+		}
+
+		db.Delete(&task)
+
+		c.JSON(200, gin.H{"message": "Task deleted"})
+	})
+
+	r.GET("/tasks/status/:status", func(c *gin.Context) {
+		status := c.Param("status")
+
+		var tasks []models.Task
+
+		db.Where("status = ?", status).Find(&tasks)
+
+		c.JSON(200, tasks)
+	})
+
+	r.GET("/tasks/search/:title", func(c *gin.Context) {
+		title := c.Param("title")
+
+		var tasks []models.Task
+
+		db.Where("title ILIKE ?", "%"+title+"%").Find(&tasks)
+
+		c.JSON(200, tasks)
+	})
+
+	r.GET("/tasks/count", func(c *gin.Context) {
+		var count int64
+
+		db.Model(&models.Task{}).Count(&count)
+
+		c.JSON(200, gin.H{
+			"count": count,
+		})
+	})
+
+	r.GET("/tasks/done", func(c *gin.Context) {
+		var tasks []models.Task
+
+		db.Where("status = ?", "done").Find(&tasks)
+
+		c.JSON(200, tasks)
+	})
+
+	r.DELETE("/tasks/done", func(c *gin.Context) {
+		db.Where("status = ?", "done").Delete(&models.Task{})
+
+		c.JSON(200, gin.H{
+			"message": "All done tasks deleted",
+		})
+	})
+
+	r.Run(":8082")
+
 }
---
+++ models/task.go
@@ -0,0 +1,8 @@
+package models
+
+type Task struct {
+	ID          uint   `json:"id" gorm:"primaryKey"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Status      string `json:"status"`
+}
